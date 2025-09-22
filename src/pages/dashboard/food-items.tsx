3d1eeaec815959a2c772b51ef7a375f36893d823
J-sense <jishan1873@gmail.com>
2025-09-22 10:06:39 +0600

    fix responsive issue of cart page and items bug fix

--- src/pages/dashboard/food-items.tsx
+++ src/pages/dashboard/food-items.tsx
@@ -21,26 +21,36 @@ type Props = {
 
 export const FoodItems = ({ item, showFood }: Props) => {
   return (
-    <div
-      onClick={() => showFood(item.id)}
-      className={cn(
-        "aspect-video md:aspect-[1/1.2] bg-sidebar flex flex-col gap-y-2 items-stretch justify-center rounded-lg shadow-sm p-4 select-none cursor-pointer"
-      )}
-    >
-      <div className="aspect-video md:aspect-square rounded-xl overflow-hidden flex justify-center items-center object-contain">
-        <img
-          src={item.image1}
-          alt={item.item_name}
-          className="object-cover w-full  h-full "
-        />
-      </div>
-      <p className="text-icon-active/80 text-wrap font-medium">
-        {item.item_name}
-      </p>
-      <p className="text-icon-active text-wrap text-start font-bold text-2xl">
-        {`$${item.price}`}
-        <span className="text-sm font-normal">/ {item.category_name}</span>
-      </p>
-    </div>
+    <>
+      {item.availability ? (
+        <div>
+          <>
+            <div
+              onClick={() => showFood(item.id)}
+              className={cn(
+                "aspect-video md:aspect-[1/1.2] bg-sidebar flex flex-col gap-y-2 items-stretch justify-center rounded-lg shadow-sm p-4 select-none cursor-pointer"
+              )}
+            >
+              <div className="aspect-video md:aspect-square rounded-xl overflow-hidden flex justify-center items-center object-contain">
+                <img
+                  src={item.image1}
+                  alt={item.item_name}
+                  className="object-cover w-full  h-full "
+                />
+              </div>
+              <p className="text-icon-active/80 text-wrap font-medium">
+                {item.item_name}
+              </p>
+              <p className="text-icon-active text-wrap text-start font-bold text-2xl">
+                {`$${item.price}`}
+                <span className="text-sm font-normal">
+                  / {item.category_name}
+                </span>
+              </p>
+            </div>
+          </>
+        </div>
+      ) : null}
+    </>
   );
 };
