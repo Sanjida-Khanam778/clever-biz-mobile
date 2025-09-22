3bac5d05e60d7db021c89a979dabfe06ad44eccb
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-09-22 14:49:26 +0600

    fix: added  modal closing X button

--- src/pages/dashboard/outlet-sheet.tsx
+++ src/pages/dashboard/outlet-sheet.tsx
@@ -1,5 +1,5 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Outlet } from "react-router";
 
 type OutletSheetProps = {
@@ -15,6 +15,11 @@ export const OutletSheet = ({ open, onOpenChange }: OutletSheetProps) => {
       </SheetTrigger>
 
       <SheetContent side="right" className="p-6 overflow-y-auto">
+        <div className="flex justify-end ">
+          <button aria-label={"close"} onClick={() => onOpenChange(false)}>
+            <X />
+          </button>
+        </div>
         <Outlet />
       </SheetContent>
     </Sheet>
