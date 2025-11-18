681b2b221c4dcc8dc389530a619fa6fbfa87c9f1
J-sense <jishan1873@gmail.com>
2025-11-18 10:19:24 +0600

    finishing touch.complete alhamdulillah

--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -291,7 +291,7 @@ function StatusBadge({
 }) {
   const classes = {
     ok: "bg-green-100 text-green-800",
-    warn: "bg-yellow-100 text-yellow-800",
+    warn: "bg-yellow-100 text-yellow-900",
     info: "bg-gray-100 text-gray-800",
     err: "bg-red-100 text-red-800",
   }[tone];
